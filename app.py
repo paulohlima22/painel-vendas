3aa4e82d0ad1dc18f1bce3f27f104e0e3514f5d7
Paulo Lima <phl.lhp@hotmail.com>
2025-06-27 10:23:20 -0300

    Ajustando porta para Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session
 import yagmail
+import os
 
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta'
@@ -30,4 +31,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
