bb250b419a8dc2a2d6db861dc4cb1f12af884f0f
Paulo Lima <phl.lhp@hotmail.com>
2025-07-26 19:43:58 -0300

    Subindo arquivos completos para o Render

--- users.py
+++ users.py
@@ -13,3 +13,12 @@ def carregar_usuarios():
 def salvar_usuarios(usuarios):
     with open(USERS_FILE, 'w') as f:
         json.dump(usuarios, f, indent=4)
+
+# Código utilitário para terminal
+if __name__ == '__main__':
+    senha = input("Digite a nova senha: ")
+    email = input("Email do usuário: ")
+    usuarios = carregar_usuarios()
+    usuarios[email] = generate_password_hash(senha)
+    salvar_usuarios(usuarios)
+    print("Senha cadastrada ou atualizada com sucesso.")
